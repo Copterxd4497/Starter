a3c98cc67a5bc5e3a585d7ae4039149137a47fc7
Copter <copterpanyphon@gmail.com>
2024-10-20 23:21:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ console.log(textIn);
 
 const textOut = `This is what we know about the avocado ${textIn}.\nCreated on ${Date.now()}`;
 fs.writeFileSync('./txt/output.txt', textOut);
-console.log('File written!');
+console.log('File written!')
 
 //Non-blocking, asynchronous way
