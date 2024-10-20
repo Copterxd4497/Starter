e61a1cf5058825ed53cffd8d46146f1549ca5ce1
Copter <copterpanyphon@gmail.com>
2024-10-21 00:44:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
+const http = require('http');
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+//File 🍕🍔🍟
 
 //blocking, synchronous way
 //const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -9,18 +14,30 @@ const fs = require('fs');
 //console.log('File written!');
 
 //Non-blocking, asynchronous way
-fs.readFile('./txt/start.', 'utf-8', (err, data1) => {
-    if (err) return console.log('ERROR! 💀💀💀')
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-        console.log(data2);
-        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-            console.log(data3);
+//fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//    if (err) return console.log('ERROR! 💀💀💀')
+//    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//        console.log(data2);
+//        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//            console.log(data3);
+//
+//            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//                console.log('Your file has been written 😍');
+//            })
+//        });
+//    });
+//});
+//console.log('Will read file!');
 
-            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-                console.log('Your file has been written 😍');
-            })
-        });
-    });
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+//server 🍕🍔🍟
+const server = http.createServer((req, res) => {
+    console.log(req);
+    res.end('Hello from the server!');
 });
 
-console.log('Will read file!');
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000');
+});
