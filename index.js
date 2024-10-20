329dceb060be2da2b5ac252bcf8f9ce64d6c6710
Copter <copterpanyphon@gmail.com>
2024-10-20 23:58:29 +0700

    nothing

--- index.js
+++ index.js
@@ -9,9 +9,18 @@ const fs = require('fs');
 //console.log('File written!');
 
 //Non-blocking, asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+fs.readFile('./txt/start.', 'utf-8', (err, data1) => {
+    if (err) return console.log('ERROR! 💀💀💀')
     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
         console.log(data2);
-    })
-})
-console.log('Will read file!')
+        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+            console.log(data3);
+
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+                console.log('Your file has been written 😍');
+            })
+        });
+    });
+});
+
+console.log('Will read file!');
