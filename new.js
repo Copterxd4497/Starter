7bc23d8e81098ee1a2c40ac9fa57f95263e731ca
Copter <copterpanyphon@gmail.com>
2024-10-21 11:09:59 +0700

    dfsg

--- new.js
+++ new.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
 
-const one = fs.readFileSync('./txt/input.txt', 'utf-8');
-fs.writeFileSync('./txt/two.txt', one);
+const one = fs.readFileSync('./txt/fuck.txt', 'utf-8');
+const two = fs.readFileSync('./txt/off.txt', 'utf-8');
+fs.writeFileSync('./txt/two.txt', `This is only thing we know about this file\n${one}\n${two}`);
